1bd3be1495747fe2a08492490456f90acbeb702d
Константин Евлампиев <kevlamiev@gmail.com>
2022-05-15 11:37:06 +0700

    initial commit

---
+++ GeekbrainsAlgorithmsIntro/HomeWorkMenu.cs
@@ -0,0 +1,25 @@
+namespace GeekbrainsAlgorithmsIntro;
+
+public struct MenuItem
+{
+    public string Dewscription;
+
+}
+
+public class HomeWorkMenu
+{
+    private (int a, string b)[] _menuList; 
+    public void Init()
+    {
+        
+    }
+
+    public void Run()
+    {
+        
+    }
+
+    public void Done()
+    {
+    }
+}
